61c9ac0641ccaa36f05b8b6eb44820705326903d
Eren_1511619 <eren_1511619@gmail.com>
2023-01-09 17:34:03 +0900

    Add TenKeyFragment

--- app/src/main/java/com/example/fragment_app/Data/Database/FMDatabase.java
+++ app/src/main/java/com/example/fragment_app/Data/Database/FMDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {StudentClassModel.class}, version = 2)
+@Database(entities = {StudentClassModel.class}, version = 1)
 public abstract class FMDatabase extends RoomDatabase {
 
     private static final String DATABASE_NAME = "info_fm.db";
